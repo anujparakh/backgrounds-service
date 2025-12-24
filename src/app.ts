4642776ab02bff298b025c7bf36aa71d29835e05
Anuj Parakh <anuj@anujinfotech.com>
2025-12-23 22:53:47 -0600

    Updated cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { routes } from './routes';
 import { Env } from './types';
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use('/*', cors());
 
 app.route('/', routes);
 
